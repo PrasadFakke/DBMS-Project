a24993a1e53552606995fb97e804f911fc6d8e05
Prasad Fakke <prasadfakke2006@gmail.com>
2025-11-05 19:51:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://dbms-exp-10.onrender.com/';
+const API_URL = 'http://localhost:3000/api';
 
 // ---------- ELEMENTS ----------
 const loginPage   = document.getElementById('loginPage');
@@ -338,4 +338,5 @@ document.getElementById('scheduleId').addEventListener('input', () => {
 // ---------- START ----------
 
 checkAuth();
+
 
